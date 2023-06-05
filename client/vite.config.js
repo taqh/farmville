afb1b8fe9476d6d0a289e66d331bb67c51b0fb75
Dom-iha <taqib.ibrahim@gmail.com>
2023-06-05 12:17:51 +0100

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+        {
+            // this is required for the SCSS modules
+            find: /^~(.*)$/,
+            replacement: '$1',
+        },
+    ],
+},
 })
